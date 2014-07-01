fa01542b068ab90e1d89a4c469e6a84f03279936
Josef Sauter <Josef.Sauter@gmail.com>
2014-07-01 00:17:17 -0600

    monty_hall task

--- 01_basics.rb
+++ 01_basics.rb
@@ -11,11 +11,31 @@ module Task1
 	
 		return false
 	end
-	
-	def self.changed
-		puts "Fuck"
-	end
+		
 end
 
+module MontyHall
+	#Task 2: Implement the Monty Hall Game
+	# http://en.wikipedia.org/wiki/Monty_Hall_problem
+	# * the game argument is an array with the following properties:
+	# 	1) The array contains only String Objects
+	# 	2) The strings in the array are only: "GOAT" or "CAR"
+	# 	3) There is only one "CAR" in each game. All others are "GOAT"
+	# * The program asks the user to enter a number between 1 and n. Remember that n may be variable. How can you determine n from the game argument?
+	# * The user enters HIS CHOICE. The result is not shown
+	# * The program chooses another door that IS NOT the "CAR". The result is shown.
+	# * The program asks the user to enter HIS FINAL CHOICE. It can be the previous choice. It cannot be the opened door shown by the program.
+	# *	The user enters HIS FINAL choice and the result is shown.
+	# * If the result is a "CAR", the program tells the user "YOU WIN!"
+	# * If the result is a "GOAT", the program tells the user "YOU LOOSE!"
+	def self.play(game)
+		# See the documentation for the class Array. (google ruby Array)
+		# Use the method puts to output text
+		# Use the method gets to recive user input
+		# To iterate an array, you may use the method .each 
+	end
+	
+	
+end
 
 
